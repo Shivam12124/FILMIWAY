e132a3fb93a849d38003096bccb7025b320738ad
Shivam12124 <shivam9893815197@gmail.com>
2025-09-29 11:00:23 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 🔥 STATIC EXPORT FOR NETLIFY - WORKS WITH SSG
+  output: 'export',
+  trailingSlash: true,
+  
   // 🔥 MINIMAL CONFIG - NO FANCY STUFF
   reactStrictMode: false,
   
   images: {
     unoptimized: true,
-    domains: ['image.tmdb.org']
+    domains: [
+      'image.tmdb.org',
+      'm.media-amazon.com',
+      'www.themoviedb.org'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
   },
   
   env: {
