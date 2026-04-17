228fc34e76452a50a63b34fd157831f6a7ab1882
Shivam12124 <shivam9893815197@gmail.com>
2026-04-17 09:16:27 +0530

    deploy

--- pages/_app.js
+++ pages/_app.js
@@ -105,15 +105,15 @@ export default function App({ Component, pageProps }) {
       </Head>
 
       {/* ⚡ OPTIMIZED: Load Clarity Analytics without blocking the main thread */}
-      <Script id="microsoft-clarity" strategy="lazyOnload">
-        {`
+      <Script id="microsoft-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
           (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "u1n9jixukw");
-        `}
-      </Script>
+        `
+      }} />
 
       {/* ✅ Wrap Component in Main with Font Variables */}
       <main className={`${bebas.variable} ${montserrat.variable} font-sans`}>
