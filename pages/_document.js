587f4408cd08258662ddf9d9113eaa769a769c06
Shivam12124 <shivam9893815197@gmail.com>
2026-05-01 10:37:09 +0530

    deploy

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* ⚡ OPTIMIZED: Preconnect */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://image.tmdb.org" crossOrigin="anonymous" />
 
         {/* Global CSS */}
