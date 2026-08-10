7fe3ceb76216859195c5d7925e005125f4f41c76
Shivam12124 <shivam9893815197@gmail.com>
2026-08-10 17:05:02 +0530

    deploy

--- generate-new-sitemap.js
+++ generate-new-sitemap.js
@@ -8,12 +8,25 @@ const sitemapPath2 = path.join(__dirname, 'public', 'sitemap-movies-v2.xml');
 // Read our new Master Database
 const movies = JSON.parse(fs.readFileSync(masterDbPath, 'utf8'));
 
-// Get the current date in ISO format to tell Google these pages were JUST updated!
-const currentDate = new Date().toISOString();
+// Aug Dates ISO Mapper matching pages/movie/[slug]/skip-timestamps.js
+const augIsoDates = [
+    "2026-08-01T12:00:00.000Z",
+    "2026-08-02T12:00:00.000Z",
+    "2026-08-03T12:00:00.000Z",
+    "2026-08-04T12:00:00.000Z",
+    "2026-08-05T12:00:00.000Z",
+    "2026-08-06T12:00:00.000Z",
+    "2026-08-07T12:00:00.000Z",
+    "2026-08-08T12:00:00.000Z",
+    "2026-08-09T12:00:00.000Z",
+    "2026-08-10T12:00:00.000Z"
+];
 
-// Generate the XML for each movie using the new flat slug
+// Generate the XML for each movie using the new flat slug and deterministic lastmod
 const sitemapEntries = movies.map(movie => {
-    return `  <url>\n    <loc>https://www.filmiway.com/movie/${movie.slug}/skip-timestamps</loc>\n    <lastmod>${currentDate}</lastmod>\n    <changefreq>daily</changefreq>\n    <priority>0.8</priority>\n  </url>`;
+    const dateSeed = Math.abs(Number(movie.tmdbId || 0) + (movie.Title || '').length);
+    const lastmodDate = augIsoDates[dateSeed % augIsoDates.length];
+    return `  <url>\n    <loc>https://www.filmiway.com/movie/${movie.slug}/skip-timestamps</loc>\n    <lastmod>${lastmodDate}</lastmod>\n    <changefreq>daily</changefreq>\n    <priority>0.8</priority>\n  </url>`;
 });
 
 // Wrap it in the official Google Sitemap XML tags
